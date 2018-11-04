b4a0ce39c79f6b0466b5648a0b75ceb826aee707
doderick <doderick@outlook.com>
2018-11-04 10:05:45 +0800

    Git Articles

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import ls from '../utils/localStorage'
 import router from '../router'
 import * as moreActions from './actions'
+import * as moreGetters from './getters'
 
 Vue.use(Vuex)
 
@@ -67,9 +68,9 @@ const actions = {
 // 添加 getters
 const getters = {
   // 第一参数是 state，因为要传 id，所以这里返回一个函数
-  getArticleById: (state) => (id) => {
+  getArticleById: (state, getters) => (id) => {
     // 从仓库获取所有文章
-    let articles = state.articles
+    let articles = getters.computedArticles
 
     // 所有文章是一个数组时
     if (Array.isArray(articles)) {
@@ -81,7 +82,9 @@ const getters = {
       // 返回 null
       return null
     }
-  }
+  },
+  // 混入 moreGetters, 你可以理解为 getters = Object.assign(getters, moreGetters)
+  ...moreGetters
 }
 
 const store = new Vuex.Store({
